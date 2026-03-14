99b0f57b4b006e51c7745f986500a016ea825b8f
natevitz <natevitz@uw.edu>
2026-03-13 17:14:15 -0700

    Resolve merge conflict by removing markers

--- src/FinalProject/FinalProject.ino
+++ src/FinalProject/FinalProject.ino
@@ -306,10 +306,6 @@ void SecurityController_Task(void *pvParameters) {
                         holdingDisplay     = true;
                         displayHoldStartMs = now;
                     }
-<<<<<<< HEAD
-                    
-=======
->>>>>>> 885e5c7 (fixed line length)
                     break;
 
                 case STATE_ALARM:
